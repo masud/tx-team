5e1ed855bf55a82a796aa9c3578ac340108e433b
masud <masud.ahmed92@gmail.com>
2015-04-08 12:09:02 +0600

    plugin file added

---
+++ tx-team.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: TX Team
+Plugin URI: http://themexpert.com/wordpress-plugins/xpert-team
+Version: 1.0
+Author: ThemeXpert
+Authro URI : http://www.themexpert.com
+Description: Supercharge your WordPress team plugin
+License: GPLv2 or later
+Text Domain: xt
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
