fc92c72ddc5117be8ee96c4706142e3ca6ac078f
masud <masud.ahmed92@gmail.com>
2015-04-08 17:16:35 +0600

    metabox added

--- tx-team.php
+++ tx-team.php
@@ -12,11 +12,41 @@ Text Domain: xt
 
 
 
+add_action('init',function(){
+	register_post_type('products',[
+		'label'=> 'Products',
+		'show_ui' => true,
+		'has_archive' => true
+		]);
+
+});
 
 
+add_action('add_meta_boxes',function(){
+	add_meta_box(
+		'more-details',
+		'More Details',
+		function(){},
+		'products'
+		);
+});
 
 
+// function prfx_custom_meta() {
+//     add_meta_box( 
 
+//     	'custom_meta_box_id',
+//     	'Product Section',
+//     	'meta_box_callback_funct',
+//     	'post',
+//     	'normal'
+//      );
+// }
+// add_action( 'add_meta_boxes', 'prfx_custom_meta' );
+
+// function meta_box_callback_funct(){
+	
+// }
 
 
 
